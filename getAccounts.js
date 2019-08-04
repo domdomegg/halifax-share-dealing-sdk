6d0284364921ee77b703b89653f56afed34bc3a1
Adam Jones <jones_adam@rocketmail.com>
2019-08-04 23:45:47 +0100

    Create getAccounts api

---
+++ getAccounts.js
@@ -0,0 +1,22 @@
+const urlBuilder = require('./urlBuilder')
+const request = require('./requestAgent')
+const log = require('./promiseLogger')
+
+module.exports = (config) => () =>
+  request(urlBuilder(config).SDHome)
+    .then(log("Got account details"))
+    .then(response => {
+      const accountContainers = response.$('.acct-overview-container').get()
+
+      return accountContainers.map((accountContainerElem) => {
+        const accountContainer = response.$(accountContainerElem)
+        const tds = response.$('td', accountContainerElem)
+
+        return {
+          name: accountContainer.find('a').first().text(),
+          availableToInvest: tds.eq(0).text(),
+          totalSecurities: tds.eq(1).text(),
+          totalValue: tds.eq(2).text()
+        }
+      })
+    })
